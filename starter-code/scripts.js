6483ec3acc978232e7c540a768f6d7dc448aecf8
Lee Wilson <indefinitelee@gmail.com>
2016-10-01 19:33:56 -0400

    Add files via upload

--- starter-code/scripts.js
+++ starter-code/scripts.js
@@ -1,6 +1,7 @@
 console.log('javascript connected!')
 var count1 = 0;
 var count2 = 0;
+
 // 1. Get two divs to move on different key presses;
   $(function(){
     $(document).keydown(function(e){
@@ -38,7 +39,6 @@ $( '#start' ).click(function() {
 //       });
 //     });
 // });
-
 //  $(document).load (function() {
 //     $("#start").click(function(){
 //         alert("button");
@@ -55,21 +55,26 @@ $( '#start' ).click(function() {
 //     left: '+=500'
 //     });
 // });
-// @media only screen and (max-device-width: 800px) {
-
-// #header {display:none;}
-
-// }
+// tried all these and no worky.
 
 // 3. Get them all to stop at edge of screen responsive
-
+// how?
 
 // 4 create a 'win state' first div to hit end wins and alerts player.
 //get X coordinate of rabbit
 //compare to X coordinate of box1 and box2 on keydown if === break winner else break??
 //or actually, count clicks whichever reaches backgroudn width/50 first wins
-var winPos = $('div').innerWidth();
+$(function(){
+  var winPos = $('#background').width();
   console.log(winPos);
+
+if (count1/50 > winPos)
+    alert('Homer Wins!');
+   else if (count2/50 > winPos)
+  alert('Barney Wins!');
+});
+//but since user can scroll past current window size this won't work.
+//that's why #3 was #3.
 
 // 5. 'start page' with start button slides up after click and starts rabbit
 // $( "p" ).click(function() {
